ca81f0d53a7bb77bbeb4348d1396418817936f6a
Per <phjorgensenn@gmail.com>
2023-12-06 10:00:33 +0100

    Make some progress on day 1, part 2

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ fn main() {
 }
 
 fn day1_part2() {
-    let file = fs::read_to_string("src/data/aoc1.txt").unwrap();
+    let file = fs::read_to_string("src/data/day_1.txt").unwrap();
 
     let lines: u32 = file
         .lines()
@@ -32,12 +32,44 @@ fn day1_part2() {
     println!("{:?}", lines);
 }
 
-fn replace_word_with_digit(line: &str) -> &str {
-    let string_digits = vec![
-        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+fn replace_word_with_digit(line: &str) -> String {
+    let mut new_string: String = line.into();
+
+    let first = find_first(line);
+    println!("{:?}", first);
+
+    // Can't reverse, because one will become eno.
+    let reversed = line.chars().rev().collect::<String>();
+    let last = find_first(reversed.as_str());
+    println!("{:?}", last);
+
+    return new_string;
+}
+
+fn find_first(line: &str) -> &str {
+    // Still does not find the first match in the word, just the first match in the items array.
+    let items = vec![
+        "one", "1", "two", "2", "three", "3", "four", "4", "five", "5", "six", "6", "seven", "7",
+        "eight", "8", "nine", "9",
     ];
 
-    return "";
+    let mut found = false;
+    let mut found_item: &str = "";
+
+    items.into_iter().for_each(|item| {
+        if found {
+            return;
+        }
+
+        let found_it = line.find(item).unwrap_or(1000);
+
+        if found_it != 1000 {
+            found = true;
+            found_item = item;
+        }
+    });
+
+    return found_item;
 }
 
 fn day1_part1() {
